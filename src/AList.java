46104978bdc8cd767ee181c0b5ae7b1761589166
jonathanp2004 <jpadilla1782.2@gmail.com>
2026-02-16 11:29:25 -0600

    Finished up AList.java, and added addy , removy , and the toString methods to the Alist class.

--- src/AList.java
+++ src/AList.java
@@ -1,32 +1,52 @@
+import java.util.Arrays;
+
 public class AList {
 
     int maxSize = 10;
     int size = 0;
-    int pos = size;
-    int[] arrayList = new int[maxSize];
+    Song[] arrayList = new Song[maxSize];
 
-    public AList(int maxSize, int size, int[] arrayList) {
-
-
+    public AList() {
     }
 
-    void addy(Song p){
+    //adds song to list
+    public void addy(Song p) {
 
-        pos = size +1;
-        size++;
-        if( size == maxSize){
-            int[] newArray = new int[maxSize + 10];
+        if (size == maxSize) {
+            Song[] newArray = new Song[maxSize + 10];
             int j = 0;
             maxSize = newArray.length;
-            for(int i =0; i < size; i++){
-
+            for (int i = 0; i < size; i++) {
                 newArray[j] = arrayList[i];
                 j++;
+
             }
+            arrayList = newArray;
+        }
+        arrayList[size] = p;
+        size++;
+    }
+        public void removy(int pos){
+        //int i = pos + 1;
+        for( int i = pos; i < size-1 ; i++ ){
+            arrayList[i] = arrayList[i +1];
+           // arrayList[i + 1] = arrayList[i + 2]; only necessary to move one element
+            }
+        size = size - 1;
+        // can also do size--;
         }
 
-    }
-    void removy(int pos){
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < size; i++) {
+
+          sb.append(arrayList[i] + "\n");
+
+        }
+        return sb.toString();
 
     }
 }
+
